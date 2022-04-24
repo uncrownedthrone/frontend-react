ab01642494cbfecc986f5b014b461f91bf94ce58
Justin Swiger <justin.swiger@me.com>
2022-04-24 15:48:34 -0400

    completed hoc, navdots, and sm-icons

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { images } from '../../constants'
+import { AppWrap } from '../../wrapper'
 
 import './Header.scss'
 
@@ -17,7 +18,7 @@ const Header = () => {
   }
 
   return (
-    <div id='home' className='app__header app__flex'>
+    <div className='app__header app__flex'>
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.75 }}
@@ -65,4 +66,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default AppWrap(Header, 'home')
